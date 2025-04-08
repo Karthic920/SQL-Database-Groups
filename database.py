e1ecccddd34690f4eacdb2d134a29ada0db09cfe
Karthic920 <karthicj26@kprschools.ca>
2025-04-08 09:32:42 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,26 +1,26 @@
 import sqlite3
 
 
-CREATE_BEANS_TABLE = "CREATE TABLE IF NOT EXISTS beans (id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);"
+CREATE_MOVIES_TABLE = "CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, name TEXT, year TEXT, actor TEXT, director TEXT, rating INTEGER);"
 
-INSERT_BEAN = "INSERT INTO beans(name, method, rating) VALUES (?, ?, ?);"
+INSERT_MOVIE = "INSERT INTO movies(name, year, actor, director, rating) VALUES (?, ?, ?);"
 
-GET_ALL_BEANS = "SELECT * FROM beans;"
-GET_BEANS_BY_NAME = "SELECT * FROM beans WHERE name = ?;"
-GET_BEST_PREPARATION_FOR_BEAN = """
-SELECT * FROM beans
+GET_ALL_MOVIES = "SELECT * FROM movies;"
+GET_MOVIES_BY_NAME = "SELECT * FROM movies WHERE name = ?;"
+GET_MOVIE = """
+SELECT * FROM movies
 WHERE name = ?
-ORBER BY rating DESC
+ORDER BY rating DESC
 LIMIT 1;"""
 
-GET_BEAN_RANGE = "SELECT * FROM beans WHERE rating >= ? AND ? >= rating"
+GET_MOVIE_RANGE = "SELECT * FROM movies WHERE rating >= ? AND ? >= rating ORDER BY rating DESC"
 
-DELETE_BEAN_BY_NAME = """
-DELETE FROM beans
+DELETE_MOVIE_BY_NAME = """
+DELETE FROM movies
 WHERE name = ? """
 
-DELETE_BEAN_BY_ID = """
-DELETE FROM beans
+DELETE_MOVIE_BY_ID = """
+DELETE FROM movies
 WHERE id = ?  """
 
 
@@ -30,34 +30,34 @@ def connect():
 
 def create_tables(connection):
     with connection:
-        connection.execute(CREATE_BEANS_TABLE)
+        connection.execute(CREATE_MOVIES_TABLE)
 
 
-def add_bean(connection, name, method, rating):
+def add_movie(connection, name, year, rating):
     with connection:
-        connection.execute(INSERT_BEAN, (name, method, rating))
+        connection.execute(INSERT_MOVIE, (name, year, rating))
 
 
-def get_all_beans(connection):
+def get_all_movies(connection):
     with connection:
-        return connection.execute(GET_ALL_BEANS).fetchall()
+        return connection.execute(GET_ALL_MOVIES).fetchall()
 
-def get_beans_by_name(connection, name):
+def get_movies_by_name(connection, name):
     with connection:
-        return connection.execute(GET_BEANS_BY_NAME, (name,)).fetchall()
+        return connection.execute(GET_MOVIES_BY_NAME, (name,)).fetchall()
 
-def get_best_preparation_for_bean(connection, name):
+def get_movie(connection, name):
     with connection:
-        return connection.execute(GET_BEST_PREPARATION_FOR_BEAN, (name,)).fetchone()
+        return connection.execute(GET_MOVIE, (name,)).fetchone()
 
-def get_bean_range(connection, range1, range2):
+def get_movie_range(connection, range1, range2):
     with connection:
-        return connection.execute(GET_BEAN_RANGE, (range1, range2)).fetchall()
+        return connection.execute(GET_MOVIE_RANGE, (range1, range2)).fetchall()
 
-def delete_bean_by_name(connection, name):
+def delete_movie_by_name(connection, name):
     with connection:
-        connection.execute(DELETE_BEAN_BY_NAME, (name,))
+        connection.execute(DELETE_MOVIE_BY_NAME, (name,))
 
-def delete_bean_by_id(connection, bean_id):
+def delete_movie_by_id(connection, movie_id):
     with connection:
-        connection.execute(DELETE_BEAN_BY_ID, (bean_id,))
+        connection.execute(DELETE_MOVIE_BY_ID, (movie_id,))
