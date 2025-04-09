e1f6dcabfff946c9002236c28a6a09fbecb9aecc
aadith-school-account <aadiths58@kprschools.ca>
2025-04-09 08:52:27 -0400

    Update app.py
    added director/actor finding

--- app.py
+++ app.py
@@ -1,63 +1,122 @@
-import sqlite3
+import database
+
+MENU_PROMPT = """
+-- Movie Finder App --
+
+Please choose one of these options:
+
+1) Add a new movie.
+2) See all movies.
+3) Find a movie by name.
+4) See which year the movie was made
+5) Select movie rating range.
+6) Find movie by lead actor or director
+7) Delete movie.
+8) Exit.
+
+Your selection:"""
 
 
-CREATE_MOVIES_TABLE = "CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, name TEXT, year TEXT, actor TEXT, director TEXT, rating INTEGER);"
+def menu():
+    connection = database.connect()
+    database.create_tables(connection)
 
-INSERT_MOVIE = "INSERT INTO movies(name, year, actor, director, rating) VALUES (?, ?, ?, ?, ?);"
+    while (user_input := input(MENU_PROMPT)) != "8":
+        if user_input == "1":
+            prompt_add_new_movie(connection)
 
-GET_ALL_MOVIES = "SELECT * FROM movies;"
-GET_MOVIES_BY_NAME = "SELECT * FROM movies WHERE name = ?;"
-GET_MOVIE_YEAR = """
-SELECT * FROM movies
-WHERE name = ?
-ORDER BY rating DESC
-LIMIT 1;"""
+        elif  user_input == "2":
+            prompt_see_all_movies(connection)
 
-GET_MOVIE_RANGE = "SELECT * FROM movies WHERE rating >= ? AND ? >= rating ORDER BY rating DESC"
+        elif  user_input == "3":
+            prompt_find_movie(connection)
 
-DELETE_MOVIE_BY_NAME = """
-DELETE FROM movies
-WHERE name = ? """
+        elif  user_input == "4":
+            prompt_find_movie_year(connection)
 
-DELETE_MOVIE_BY_ID = """
-DELETE FROM movies
-WHERE id = ?  """
+        elif  user_input == "5":
+            prompt_movie_range(connection)
+
+        elif  user_input == "6":
+            prompt_find_movie_actor_or_director(conneciton)
+        elif user_input == "7":
+            prompt_delete_movie(connection)
+
+        else:
+            print("Invalid input, please try again!")
+
+def prompt_add_new_movie(connection):
+    name = input("Enter movie name: ")
+    year = input("Enter the year it was published: ")
+    actor = input("Enter lead actor: ")
+    director = input("Enter director: ")
+    rating = int(input("Enter your rating score (0-100): "))
+
+    database.add_movie(connection, name, year, actor, director, rating)
+
+def prompt_see_all_movies(connection):
+    movies = database.get_all_movies(connection)
+
+    for movie in movies:
+        print(f"{movie[1]} ({movie[2]}), actor: {movie[3]}, director: {movie[4]} - {movie[5]}/100")
+
+def prompt_find_movie(connection):
+    name = input("Enter movie name to find: ")
+    movies = database.get_movies_by_name(connection, name)
+
+    for movie in movies:
+        print(f"{movie[1]} ({movie[2]}), actor: {movie[3]}, director: {movie[4]} - {movie[5]}/100")
+
+def prompt_find_movie_year(connection):
+    name = input("Enter movie name to find: ")
+    year = database.get_movie_year(connection, name)
+
+    print(f"The year {name} was published is: {year[2]}")
+
+def prompt_movie_range(connection):
+    range1 = int(input("What is the lowest rating of movie you would like?"))
+    range2 = int(input("What is the highest rating of movie you would like?"))
+    movies = database.get_movie_range(connection, range1, range2)
+
+    print("\n")
+
+    for movie in movies:
+        print(f"{movie[1]} ({movie[2]}), actor: {movie[3]}, director: {movie[4]} - {movie[5]}/100")
+
+def prompt_find_movie_actor_or_director(connection):
+    choice = input("Would you like to find the movie by actor or director?\n(1 for actor, 2 for director): ")
+
+    if choice == "1":
+        actor_name = input("What is the lead actors name?: ")
+        movies = database.get_movie_by_actor(connection, actor_name)
+
+        for movie in movies:
+            print(f"{movie[1]} ({movie[2]}), actor: {movie[3]}, director: {movie[4]} - {movie[5]}/100")
+
+    elif choice == "2":
+        director_name = input("What is the directors name?: ")
+        movies = database.get_movie_by_director(connection, director_name)
+        for movie in movies:
+            print(f"{movie[1]} ({movie[2]}), actor: {movie[3]}, director: {movie[4]} - {movie[5]}/100")
+
+        
 
 
-def connect():
-    return sqlite3.connect("data.db")
+def prompt_delete_movie(connection):
+    user_input = input("""Please choose one of these options:
+
+    1) Delete by name.
+    2) Delete by ID.
+
+    Your selection:""")
+
+    if user_input == "1":
+        name = input("Enter movie name to delete: ")
+        database.delete_movie_by_name(connection, name)
+
+    elif user_input == "2":
+        ID = int(input("Enter movie ID to delete: "))
+        database.delete_movie_by_id(connection, ID)
 
 
-def create_tables(connection):
-    with connection:
-        connection.execute(CREATE_MOVIES_TABLE)
-
-
-def add_movie(connection, name, year, actor, director, rating):
-    with connection:
-        connection.execute(INSERT_MOVIE, (name, year, actor, director, rating))
-
-
-def get_all_movies(connection):
-    with connection:
-        return connection.execute(GET_ALL_MOVIES).fetchall()
-
-def get_movies_by_name(connection, name):
-    with connection:
-        return connection.execute(GET_MOVIES_BY_NAME, (name,)).fetchall()
-
-def get_movie_year(connection, name):
-    with connection:
-        return connection.execute(GET_MOVIE_YEAR, (name,)).fetchone()
-
-def get_movie_range(connection, range1, range2):
-    with connection:
-        return connection.execute(GET_MOVIE_RANGE, (range1, range2)).fetchall()
-
-def delete_movie_by_name(connection, name):
-    with connection:
-        connection.execute(DELETE_MOVIE_BY_NAME, (name,))
-
-def delete_movie_by_id(connection, movie_id):
-    with connection:
-        connection.execute(DELETE_MOVIE_BY_ID, (movie_id,))
+menu()
