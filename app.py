cdcfa346178605a4187b419b5bf1a117656c6c9e
Karthic920 <karthicj26@kprschools.ca>
2025-04-09 08:41:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def menu():
             prompt_find_movie(connection)
 
         elif  user_input == "4":
-            prompt_find_best_method(connection)
+            prompt_find_movie_year(connection)
 
         elif  user_input == "5":
             prompt_movie_range(connection)
@@ -45,26 +45,28 @@ def menu():
 def prompt_add_new_movie(connection):
     name = input("Enter movie name: ")
     year = input("Enter the year it was published: ")
+    actor = input("Enter lead actor: ")
+    director = input("Enter director: ")
     rating = int(input("Enter your rating score (0-100): "))
 
-    database.add_movie(connection, name, year, rating)
+    database.add_movie(connection, name, year, actor, director, rating)
 
 def prompt_see_all_movies(connection):
     movies = database.get_all_movies(connection)
 
     for movie in movies:
-        print(f"{[1]} ({movie[2]}) - {movie[3]}/100")
+        print(f"{movie[1]} ({movie[2]}), actor: {movie[3]}, director: {movie[4]} - {movie[5]}/100")
 
 def prompt_find_movie(connection):
     name = input("Enter movie name to find: ")
     movies = database.get_movies_by_name(connection, name)
 
     for movie in movies:
-        print(f"{movie[1]} ({movie[2]}) - {movie[3]}/100")
+        print(f"{movie[1]} ({movie[2]}), actor: {movie[3]}, director: {movie[4]} - {movie[5]}/100")
 
-def prompt_find_best_method(connection):
+def prompt_find_movie_year(connection):
     name = input("Enter movie name to find: ")
-    year = database.get_best_perparation_for_movie(connection, name)
+    year = database.get_movie_year(connection, name)
 
     print(f"The year {name} was published is: {year[2]}")
 
@@ -76,7 +78,7 @@ def prompt_movie_range(connection):
     print("\n")
 
     for movie in movies:
-        print(f"{movie[1]} ({movie[2]}) - {movie[3]}/100")
+        print(f"{movie[1]} ({movie[2]}), actor: {movie[3]}, director: {movie[4]} - {movie[5]}/100")
 
 def prompt_delete_movie(connection):
     user_input = input("""Please choose one of these options:
